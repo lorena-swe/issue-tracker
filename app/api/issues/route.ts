438676cd16f576507d64e0a85f503249d5ffefb5
Lorena <lorenazotaj10@gmail.com>
2024-06-24 09:29:24 +0200

    Handled form submission errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,8 +3,8 @@ import { z } from 'zod';
 import prisma from "@/prisma/client";
 
 const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1)
+  title: z.string().min(1, 'A title is required.').max(255),
+  description: z.string().min(1, 'A description is required.')
 })
 
 export async function POST(request: NextRequest) {
