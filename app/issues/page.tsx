0ede2431f2132dd1c05fecb65051c5a72120fabb
Lorena <lorenazotaj10@gmail.com>
2024-06-24 10:59:37 +0200

    Built a styled Link component (w client side navigation)

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,9 +2,9 @@
 // "use client";
 
 import React from "react";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
+import { Table } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
+import Link from "../components/Link";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from "delay";
 import IssueActions from "./IssueActions";
