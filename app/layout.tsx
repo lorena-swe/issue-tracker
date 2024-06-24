7b72129688278b1d0f65c5e31b431655ccf5e587
Lorena <lorenazotaj10@gmail.com>
2024-06-24 12:46:28 +0200

    Set up ReactQuery (tanstack)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
+import { Container, Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
-import "./theme-config.css";
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Container, Theme, ThemePanel } from "@radix-ui/themes";
-import NavBar from "./NavBar";
 import AuthProvider from "./auth/Provider";
+import "./globals.css";
+import NavBar from "./NavBar";
+import QueryClientProvider from "./QueryClientProvider";
+import "./theme-config.css";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,15 +27,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <AuthProvider>
-          <Theme accentColor="violet">
-            <NavBar />
-            <main className="p-5">
-              <Container>{children}</Container>
-            </main>
-            {/* <ThemePanel /> */}
-          </Theme>
-        </AuthProvider>
+        <QueryClientProvider>
+          <AuthProvider>
+            <Theme accentColor="violet">
+              <NavBar />
+              <main className="p-5">
+                <Container>{children}</Container>
+              </main>
+              {/* <ThemePanel /> */}
+            </Theme>
+          </AuthProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
