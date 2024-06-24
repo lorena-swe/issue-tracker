2d6e0c088c27b4dd3e238ec85694bf03e7c3061a
Lorena <lorenazotaj10@gmail.com>
2024-06-24 11:01:55 +0200

    Added additional loading skeletons

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
+import delay from "delay";
 
 interface Props {
   params: { id: string };
@@ -23,6 +24,8 @@ const IssueDetailPage = async ({ params }: Props) => {
   if (!issue) {
     notFound();
   }
+
+  await delay(2000);
 
   return (
     <div>
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,17 +1,15 @@
 /* eslint-disable @next/next/no-async-client-component */
 // "use client";
 
-import React from "react";
-import { Table } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
-import Link from "../components/Link";
+import { Table } from "@radix-ui/themes";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
+import Link from "../components/Link";
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
+  // await delay(2000);
 
   return (
     <div>
