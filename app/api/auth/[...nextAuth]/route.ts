d725e23b7c839697f7d5cdcd829a1a2c2224ead5
Lorena <lorenazotaj10@gmail.com>
2024-06-24 11:46:59 +0200

    Set up NextAuth

---
+++ app/api/auth/[...nextAuth]/route.ts
@@ -0,0 +1,9 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [],
+}
+
+export default NextAuth(authOptions)
